a17e9c029de068d7a5f3e7a8d19564b130e6e5f0
Cyoungju <dudwn3528@gmail.com>
2023-09-12 16:44:34 +0900

    PT last commit mabye

--- InsaengRPG/src/Menu/PT.java
+++ InsaengRPG/src/Menu/PT.java
@@ -1,17 +1,30 @@
 package Main;
 import Main.ObjectManager;
 import Menu.Scene;
+import Menu.SceneManager;
 
 import java.util.Scanner;
 
 public class PT extends Scene {
+    private String homeParty = "홈파티";
+    private String villageParty = "마을잔치";
+    private String kingdomParty = "왕궁파티";
+
+    private String printName;
+    private int money ,social, stress ;
+
+
     private Scanner scanner = null;
     private int input(){
         int num = 0;
-
         menuTxt();
-        System.out.println("입력 : ");
-        num = scanner.nextInt();
+
+        try {
+            System.out.println("파티 종류 입력 : ");
+            num = scanner.nextInt();
+        }catch (Exception e){
+            System.out.println("에러");
+        }
 
         return num;
     }
@@ -20,9 +33,9 @@ public class PT extends Scene {
 
     public void menuTxt(){
         System.out.println("====== 파티 종류를 선택해 주세요 ======");
-        System.out.println("1. 홈파티");
-        System.out.println("2. 마을잔치");
-        System.out.println("3. 왕국파티");
+        System.out.println("1. "+homeParty);
+        System.out.println("2. "+villageParty);
+        System.out.println("3. "+kingdomParty);
         System.out.println("===================================");
     }
 
@@ -45,34 +58,40 @@ public class PT extends Scene {
             }
         }
 
-
         switch (i) {
             case 1:
-                System.out.println("홈파티를 선택하셨습니다.");
-                System.out.println("돈 - 5\n" + "사교성 + 3\n" + "스트레스 + 2\n");
-                user.setSociability(user.getSociability() + 3);
-                user.setPTSD(user.getPTSD() + 2);
+                printName =  homeParty;
+                money = 5;
+                social = 3;
+                stress = 2;
                 break;
 
             case 2:
-                System.out.println("마을잔치를 선택하셨습니다.");
-                System.out.println("돈 - 10\n" + "사교성 + 5\n" + "스트레스 + 2\n");
-                user.setSociability(user.getSociability() + 5);
-                user.setPTSD(user.getPTSD() + 2);
+                printName =  villageParty;
+                money = 10;
+                social = 5;
+                stress = 2;
                 break;
 
             case 3:
-                System.out.println("왕국파티를 선택하셨습니다.");
-                System.out.println("돈 - 15\n" + "사교성 + 7\n" + "스트레스 + 2\n");
-                user.setSociability(user.getSociability() + 7);
-                user.setPTSD(user.getPTSD() + 2);
+                printName =  kingdomParty;
+                money = 15;
+                social = 7;
+                stress = 2;
                 break;
         }
+
+        user.setWealth(user.getWealth() - money);
+        user.setSociability(user.getSociability() + social);
+        user.setPTSD(user.getPTSD() + stress);
+
     }
 
     @Override
     public void render() {
-        menuTxt();
+        System.out.println(printName+"를 선택하셨습니다.");
+        System.out.println("돈 -"+money+ " 사교성 +"+social + "스트레스 +"+stress);
+        SceneManager.getInstance().setScene();
     }
 }
 
